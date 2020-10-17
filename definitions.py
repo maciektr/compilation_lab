893f636e3b1d2dc4b8a0721083e11fbca3b50aa1
maciektr <mtratnowiecki@gmail.com>
2020-10-17 17:43:47 +0200

    Fix real numbers and small changes to other

--- definitions.py
+++ definitions.py
@@ -4,13 +4,17 @@ reserved = {
     'zeros': 'ZEROS',
     'ones': 'ONES',
     'eye': 'EYE',
+    'if': 'IF',
+    'while': 'WHILE',
+    'for': 'FOR',
+    'else': 'ELSE',
 }
 
 tokens = (
     'ID',           #ISSUE: Assigns "IF", "WHILE", etc. as IDs
     'INTNUM',       #ISSUE: ASSIGNS REAL AS INTNUM + '.' + INTNUM
     'REAL',         #ISSUE: ASSIGNS REAL AS INTNUM + '.' + INTNUM
-    'PLUS',         #DONE, NOT TESTED  
+    'PLUS',         #DONE, NOT TESTED
     'MINUS',        #DONE, NOT TESTED
     'MUL',          #DONE, NOT TESTED
     'DIVIDE',       #DONE, NOT TESTED
@@ -24,25 +28,18 @@ tokens = (
     'DOTMINUS',     #DONE, TESTED
     'DOTMUL',       #DONE, NOT TESTED
     'DOTDIV',       #DONE, NOT TESTED
-    'ASSIGN',       #DONE, NOT TESTED   
-    'PLUSASGN',     #DONE, NOT TESTED 
-    'MINUSASGN',    #DONE, NOT TESTED 
-    'MULASGN',      #DONE, NOT TESTED 
-    'DIVASGN',      #DONE, NOT TESTED   
-    'LESSTHAN',     #DONE, NOT TESTED 
-    'GREATERTHAN',  #DONE, NOT TESTED 
-    'LESSOREQ',     #DONE, NOT TESTED 
-    'GREATEROREQ',  #DONE, NOT TESTED 
-    'NOTEQ',        #DONE, NOT TESTED 
-    'EQUAL',        #DONE, NOT TESTED 
+    'PLUSASGN',     #DONE, NOT TESTED
+    'SUBASSIGN',    #DONE, NOT TESTED
+    'MULASSIGN',      #DONE, NOT TESTED
+    'DIVASSIGN',      #DONE, NOT TESTED
+    'LESSTHAN',     #DONE, NOT TESTED
+    'GREATERTHAN',  #DONE, NOT TESTED
+    'LESSOREQ',     #DONE, NOT TESTED
+    'GREATEROREQ',  #DONE, NOT TESTED
+    'NOTEQ',        #DONE, NOT TESTED
+    'EQUAL',        #DONE, NOT TESTED
     'RANGEOP',      #DONE, NOT TESTED
     'TRANSPOSE',    #DONE, NOT TESTED
-    'COMMA',        #DONE, NOT TESTED
-    'SEMICOLON',    #DONE, NOT TESTED
-    'IF',           #
-    'ELSE',         #
-    'FOR',          #
-    'WHILE',        #
     'BREAK',        #
     'CONTINUE',     #
     'RETURN',       #
@@ -51,12 +48,6 @@ tokens = (
     *reserved.values()
 )
 
-t_PLUS    = r'\+'
-t_MINUS   = r'-'
-t_MUL   = r'\*'
-t_DIVIDE  = r'/'
-t_LPAREN  = r'\('
-t_RPAREN  = r'\)'
 t_LSQPAREN  = r'\['
 t_RSQPAREN  = r'\]'
 t_LBRACK  = r'\{'
@@ -65,11 +56,10 @@ t_DOTPLUS = r'\.\+'
 t_DOTMINUS = r'\.-'
 t_DOTMUL = r'\.\*'
 t_DOTDIV = r'\./'
-t_ASSIGN = r'\='
 t_PLUSASGN = r'\+\='
-t_MINUSASGN = r'-\='
-t_MULASGN = r'\*\='
-t_DIVASGN = r'/\='
+t_SUBASSIGN = r'-\='
+t_MULASSIGN = r'\*\='
+t_DIVASSIGN = r'/\='
 t_LESSTHAN = r'\<'
 t_GREATERTHAN = r'\>'
 t_LESSOREQ = r'\<\='
@@ -77,9 +67,6 @@ t_GREATEROREQ = r'\>\='
 t_NOTEQ = r'\!\='
 t_EQUAL = r'\=\='
 t_RANGEOP = r'\:'
-t_TRANSPOSE = r'\''
-t_COMMA = r'\,'
-t_SEMICOLON = r'\;'
 
 def t_ID(t):
     r'[a-zA-Z_][a-zA-Z_0-9]*'
@@ -87,16 +74,18 @@ def t_ID(t):
     return t
     # doesn't work, assigns "IF", "WHILE", etc as IDs
 
-def t_INTNUM(t):
-    r'\d+'
-    t.value = int(t.value)    
-    return t
-    
 def t_REAL(t):
-    r'^-?[0-9]*\.?[0-9]+([eE][-+]?[0-9]+)?$'
-    t.value = int(t.value)    
+    r'-?([0-9]*)?\.([0-9]+([eE][-+]?[0-9]+)?)?'
+    if t.value.endswith('.'):
+        t.value = t.value[:-1]
+    t.value = float(t.value if t.value else 0)
     return t
     #doesn't work, assigns real numbers as INTNUM + '.' + INTNUM
+
+def t_INTNUM(t):
+    r'\d+'
+    t.value = int(t.value)
+    return t
 
 def t_newline(t):
     r'\n+'
@@ -105,14 +94,11 @@ def t_newline(t):
 t_ignore  = ' \t'
 t_ignore_COMMENT = r'\#.*'
 
+
+literals = [
+    '+', '-', '*', '/', '=', "'", ';', '(', ')', "'", '"', ',',
+]
+
 def t_error(t):
     print("Illegal character '%s'" % t.value[0])
     t.lexer.skip(1)
-
-
-literals = [
-    '+', '-', '*', '/', '=', "'", ';', '(', ')',
-]
-
-
-
