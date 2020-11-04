661c2bad0832a7d42493239dd95a469a51328fca
maciektr <mtratnowiecki@gmail.com>
2020-11-04 12:50:05 +0100

    Implement parser (#2)
    * Reorganize project for parser implementation
    
    * Update .gitignore
    
    * Add parser definitions
    
    * Update parser_defs.py
    
    * Update parser_defs.py
    
    * Add more parser definitions
    
    * Update parser_defs.py
    
    * Update parser defs remove errors
    
    * Update parser_defs.py
    
    * Update parser_defs.py
    
    Co-authored-by: sp9dev <sp9dev@gmail.com>

---
+++ parser.py
@@ -0,0 +1,14 @@
+from scanner_defs import tokens
+from parser_defs import *
+import ply.yacc as yacc
+
+class Parser:
+    def __init__(self, lexer):
+        self.parser = yacc.yacc()
+        self.lexer = lexer
+
+    def get_parser(self):
+        return self.parser
+
+    def parse(self, text):
+        return self.parser.parse(text, lexer=self.lexer)
