2e368a0b9f0fe0115fd0f4d999e3fd025bdd25d6
sp9dev <sp9dev@gmail.com>
2020-12-08 00:56:06 +0100

    rozgrzebane i ogólnie płaczę nad tym bo to straszne gówno jest

---
+++ typechecker/SymbolTable.py
@@ -0,0 +1,41 @@
+class Scope:
+    def __init__(self, parent=None):
+        self.dict = dict()
+        self.parent = parent
+
+    def put(self, name, symbol):
+        self.dict[name] = symbol
+    
+    def get(self, name):
+        if name in self.dict:
+            return self.dict[name]
+        if self.parent == None:
+            return None
+        return self.parent.get(name)
+
+class SymbolTable(object):
+
+    def __init__(self, parent, name): # parent scope and symbol table name
+        pass
+    #
+
+    def put(self, name, symbol): # put variable symbol or fundef under <name> entry
+        pass
+    #
+
+    def get(self, name): # get variable symbol or fundef from <name> entry
+        pass
+    #
+
+    def getParentScope(self):
+        pass
+    #
+
+    def pushScope(self, name):
+        pass
+    #
+
+    def popScope(self):
+        pass
+    #
+
